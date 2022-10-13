0d3ed38586c1feb3535f813e64e0964a41010868
nourasmahat <114978941+nourasmahat@users.noreply.github.com>
2022-10-13 15:08:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//var prenom = prompt("comment t'appelles tu ?");
+//var bonjour = "Bonjour"
+//bonjour = bonjour + " " + prenom
+//document.body.innerHTML += "<h2>" + bonjour + "</h2" 
+//+= qui implique une addition au contenu existant dans la variable, ici document.body.innerHTML
+
+//var naissance = prompt("quelle est ton annee de naissance ?")
+//var calcul = 2022 - naissance
+//document.body.innerHTML += "<h3>" + "Vous avez" + " " +calcul+ " "  + "ans" + "</h3"
+
+ function AskName() {prompt("comment t'appelles-tu?")
+return prompt};
+ document.body.innerHTML += "<h2>" + "Bonjour" + " " + AskName () + "</h2" 
+
+
+
+//function AskName ("bonjour", prompt("comment t'appelles tu ?")) {return "bonjour"+ prompt()}
+
+
+//document.querySelector('#header').innerHTML = bonjour  
+
+
+
